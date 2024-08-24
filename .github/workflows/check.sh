3b1e4d0d8bbe814c724bda4f36583c49636f0127
hkadham <36906033+hkadham@users.noreply.github.com>
2024-08-24 23:32:56 +0530

    shell script to install cowsay

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -1,1 +1,9 @@
 echo "Just checking if we can run the shell script or not"
+echo "Installing the cowsay package "
+sudo apt-get install cowsay -y
+echo "Running the binary cowsay"
+cowsay -f dragon "Run for cover,I am a fucker....ohhhhh">> fucker.txt
+echo "Check if the file exists or not"
+grep -i "fucker" fucker.txt
+echo "Open the file fucker.txt"
+cat fucker.txt
